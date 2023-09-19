7a069ee88ac27374b836007db0f12dd1956dfa2f
karanbadhwar <karanbadhwar07@gmail.com>
2023-09-19 00:17:19 -0400

    Reverse func

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -134,18 +134,20 @@ const showStats = async (req, res) => {
     { $limit: 6 },
   ]);
 
-  monthlyApplications = monthlyApplications.map((item) => {
-    const {
-      _id: { year, month },
-      count,
-    } = item;
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
 
-    const date = moment()
-      .month(month - 1)
-      .year(year)
-      .format("MMM Y");
-    return { date, count };
-  });
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
 
   res.status(StatusCodes.OK).json({
     defaultStats,
